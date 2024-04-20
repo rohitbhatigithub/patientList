6dac0ef64f40590a54cadcc68963185419f81fd5
rohitbhatigithub <rohitbhatia277@gmail.com>
2024-04-20 15:23:43 +0530

    toml

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
                                     />
                                 }
                             />
+                            <Route path="*" element={<Dashboard />} />
                         </Routes>
                     </BrowserRouter>
                 </div>
